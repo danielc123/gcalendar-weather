3088346d196b03e156a39b110f2137f05a70a321
Daniel C <danielc.123@gmail.com>
2020-07-10 17:28:31 +0200

    Relocating line conditions, it was removed feels like, since I haven't notice a different temperature in my location. Shortened Humidity by HR and removed word Wind. Relocated HR first top and Wind information lower bottom

--- weather.py
+++ weather.py
@@ -354,48 +354,33 @@ class MyDisplay:
         return True
 
     def display_conditions_line(self, label, cond, is_temp, multiplier=None):
-        y_start_position = 0.17     #start position
+        y_start_position = 0     #start position
         line_spacing_gap = 0.065    #line spacing
         conditions_text_height = 0.05
         degree_symbol_height = 0.03
         degree_symbol_y_offset = 0.001
-        x_start_position = 0.52
+        x_start_position = self.xmax 
         second_column_x_start_position = 0.69
         text_color = (255, 255, 255)
         font_name = "freesans"
 
-        if multiplier is None:
-            y_start = y_start_position
-        else:
-            y_start = (y_start_position + line_spacing_gap * multiplier)
+        y_start = (y_start_position + line_spacing_gap * multiplier)
 
         conditions_font = pygame.font.SysFont(
-            font_name, int(self.ymax * conditions_text_height), bold=1)
+            font_name, int(self.ymax * conditions_text_height), bold=0)
 
-        txt = conditions_font.render(str(label), True, text_color)
+        txt_label = conditions_font.render(str(label), True, text_color)
+        (txt_label_x, txt_label_y) = txt_label.get_size()
 
-        self.screen.blit(
-            txt, (self.xmax * x_start_position, self.ymax * y_start))
 
-        txt = conditions_font.render(str(cond), True, text_color)
-        self.screen.blit(txt, (self.xmax * second_column_x_start_position,
-                               self.ymax * y_start))
+        txt_cond = conditions_font.render(str(cond), True, text_color)
+        (txt_cond_x, txt_cond_y) = txt_cond.get_size()
+        self.screen.blit(txt_label, 
+            ( x_start_position-txt_label_x - txt_cond_x - 10, self.ymax * y_start + txt_label_y + 4))
 
-        if is_temp:
-            txt_x = txt.get_size()[0]
-            degree_font = pygame.font.SysFont(
-                font_name, int(self.ymax * degree_symbol_height), bold=1)
-            degree_txt = degree_font.render(UNICODE_DEGREE, True, text_color)
-            self.screen.blit(degree_txt, (
-                self.xmax * second_column_x_start_position + txt_x * 1.01,
-                self.ymax * (y_start + degree_symbol_y_offset)))
-            degree_letter = conditions_font.render(get_temperature_letter(),
-                                                   True, text_color)
-            degree_letter_x = degree_letter.get_size()[0]
-            self.screen.blit(degree_letter, (
-                self.xmax * second_column_x_start_position +
-                txt_x + degree_letter_x * 1.01,
-                self.ymax * (y_start + degree_symbol_y_offset)))
+        self.screen.blit(txt_cond, 
+            ( x_start_position- txt_cond_x , self.ymax * y_start + txt_label_y + 4))
+
 
     def display_subwindow(self, data, day, c_times):
         subwindow_centers = 0.125
